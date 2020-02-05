8057a496e0852ad9794881f93612e66a5718cd7e
Pawel <pawel.dabala@gmail.com>
2020-02-05 18:19:37 +1100

    Patrick's changes

---
+++ app/com/scalac/config/GithubApiConfig.scala
@@ -0,0 +1,6 @@
+package io.scalac.config
+
+case class GithubApiConfig(
+                            ghToken: String,
+                            baseUrl: String
+                          )
---
+++ app/com/scalac/model/Contributor.scala
@@ -0,0 +1,6 @@
+package io.scalac.model
+
+case class Contributor(
+                         login: String,
+                         contributions: Int
+                       )
